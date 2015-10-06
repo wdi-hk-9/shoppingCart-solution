3008f08987f4d92f52b73762d8c7be359f6fd15a
Denis Cheung <d.s.h.cheung@gmail.com>
2015-10-06 15:33:48 +0800

    feat(updateSubtotal)

--- js/application.js
+++ js/application.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-  // add item to list function
-  // calculate sub-total function
   // total price function
 
+  // add item to list function
   $('#new-item-btn').on("click", function (e) {
     e.preventDefault();
     var itemName = $('#new-name').val();
@@ -15,9 +14,33 @@ $(document).ready(function(){
     html = html + "  <td>$00.00</td>";
     html = html + "</tr>";
     $('tbody').append(html);
-
     $('form')[0].reset();
+    bindInput();
   });
+
+  // calculate sub-total function
+  function updateSubtotal(e) {
+
+    var quantityStr = e.target.value;
+    var quantityNum = parseFloat(quantityStr);
+
+    var jqueryObj = $(e.target);
+    var priceStr = jqueryObj.parent().prev().html();
+    var priceNum = parseFloat(priceStr.replace("$", ""));
+
+    var subtotal = quantityNum * priceNum;
+    jqueryObj.parent().next().html("$" + subtotal.toFixed(2));
+  }
+  function bindInput () {
+    $('tbody input').on("keyup", updateSubtotal);
+  }
+  bindInput();
+
+  // select input box
+  // add keyup/keydown
+  // attach function
+  // in function, calculate subtotal
+  // change html text to the subtotal
 
 
 });
